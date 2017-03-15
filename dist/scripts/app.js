89864fd038469d9d079da00bcc8b77a9877832e8
SKMKCGK <scott.kipp@gmail.com>
2017-03-15 11:28:02 -0400

    Add firebase

---
+++ dist/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+        });
+        $stateProvider
+         .state('landing', {
+            url: '/',
+            controller: 'LandingCtrl as landing',
+            templateUrl: '/templates/home.html'
+        })
+        
+       
+    }
+    angular
+        .module('blocChat', ['ui.router', 'firebase'])
+        .config(config);
+})();
