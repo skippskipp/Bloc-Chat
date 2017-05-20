be9df8276b43fd1e43fc3ab8a3628d1988f567d6
skippskipp <Scott@Melanies-MacBook-Pro.local>
2017-05-20 09:01:48 -0400

    Fix to username syntax in MessageService.js

--- dist/scripts/services/MessageService.js
+++ dist/scripts/services/MessageService.js
@@ -4,22 +4,23 @@
         var ref = firebase.database().ref().child("messages");
         var username = $cookies.get('blocChatCurrentUser');
         var time = firebase.database.ServerValue.TIMESTAMP;
-        
+
         MessageService.messages = [];
-        
+
         MessageService.getByRoomId = function(roomId) {
             console.log(roomId);
             var roomMessages = ref.orderByChild("roomId").equalTo(roomId);
             MessageService.messages = $firebaseArray(roomMessages);
         };
-        
+
         MessageService.sendMessage = function(messageContent, roomId) {
             if (messageContent) {
+                console.log("sendMessage called");
                 console.log(messageContent);
-            $firebaseArray(ref).$add({ username : username, content: messageContent, sentAt: time, roomId: roomId })
+            $firebaseArray(ref).$add({ username: username, content: messageContent, sentAt: time, roomId: roomId })
             }
         };
-        
+
         return MessageService;
     }
 
